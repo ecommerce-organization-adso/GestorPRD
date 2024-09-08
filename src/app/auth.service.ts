457b59e5c201ca936b6e87ef9c2e7a2b73d8958a
Felipe IO <pipe199518@hotmail.com>
2024-09-07 21:46:27 -0500

    Commit LogOut

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { ApirestService } from './servicios/apirest/apirest.service';
 import * as bcrypt from 'bcryptjs';
@@ -8,56 +8,54 @@ import * as bcrypt from 'bcryptjs';
 @Injectable({
   providedIn: 'root',
 })
-
-
-
 export class AuthService {
+  private isAuthenticatedSubject = new BehaviorSubject<boolean>(this.getInitialAuthState());
+  isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
 
+  private loginStatusSubject = new BehaviorSubject<boolean>(this.getInitialAuthState());
 
-  private isAuthenticated = false;
   constructor(private apirestService: ApirestService, private router: Router) {}
 
-
-      //FUNCION
-      login(email: string, password: string): Observable<boolean> {
-        return this.apirestService.getUsuarios().pipe(
-          map((response: any[]) => {
-            const user = response.find((user: any) => user.username === email);
-            if (user) {
-              // Usar bcrypt.compareSync para evitar problemas de retorno
-              const isMatch = bcrypt.compareSync(password, user.password);
-              if (isMatch) {
-                // Si la contraseña es correcta, guarda el ID del usuario y redirige
-                // localStorage.setItem('userId', user.id);
-                this.isAuthenticated = true;
-                this.router.navigate(['/dashboard/default']);
-                return true;
-              } else {
-                // Contraseña incorrecta
-                console.log('Contraseña incorrecta');
-                return false;
-              }
-            } else {
-              // Usuario no encontrado
-              console.log('Usuario no encontrado');
-              return false;
-            }
-          }),
-          catchError(error => {
-            console.error('Error al consultar la API', error);
-            return of(false);
-          })
-        );
-      }
-
-  logout(): void {
-/*     localStorage.removeItem('userId');
- */
-    this.isAuthenticated = false;
-    this.router.navigate(['/login']);
+  private getInitialAuthState(): boolean {
+    return localStorage.getItem('isAuthenticated') === 'true';
   }
 
-  isLoggedIn(): boolean {
-    return this.isAuthenticated || !!localStorage.getItem('userId');
+  login(email: string, password: string): Observable<boolean> {
+    return this.apirestService.getUsuarios().pipe(
+      map((response: any[]) => {
+        const user = response.find((user: any) => user.username === email);
+        if (user) {
+          const isMatch = bcrypt.compareSync(password, user.password);
+          if (isMatch) {
+            this.isAuthenticatedSubject.next(true);
+            localStorage.setItem('isAuthenticated', 'true');
+            this.router.navigate(['/dashboard/default']);
+            return true;
+          } else {
+            console.log('Contraseña incorrecta');
+            return false;
+          }
+        } else {
+          console.log('Usuario no encontrado');
+          return false;
+        }
+      }),
+      catchError(error => {
+        console.error('Error al consultar la API', error);
+        return of(false);
+      })
+    );
+  }
+
+  logout() {
+    this.isAuthenticatedSubject.next(false);
+    localStorage.removeItem('isAuthenticated');
+    this.router.navigate(['/login']);
+    console.log('Usuario deslogueado y sesión destruida');
+    this.loginStatusSubject.next(false);
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.isAuthenticated$;
   }
 }
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { ApirestService } from '../servicios/apirest/apirest.service';
+import { Observable,of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,12 +10,20 @@ import { ApirestService } from '../servicios/apirest/apirest.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.authService.isLoggedIn()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isLoggedIn().pipe(
+      map(isLoggedIn => {
+        if (isLoggedIn) {
+          return true;
+        } else {
+          this.router.navigate(['/login']);
+          return false;
+        }
+      }),
+      catchError(() => {
+        this.router.navigate(['/login']);
+        return of(false);
+      })
+    );
   }
 }
